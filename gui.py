2d810ee9d1ee91cb0e0cd658a7d10755f3a2b9d6
tunga-m <tungamarkal@gmail.com>
2026-09-12 15:07:05 -0400

    Refined "add" implementation

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import actions  # noqa: F401
 import file_manager
 
 label = sg.Text("Enter a task:")
-input_box = sg.InputText(tooltip="Type your task here")
+input_box = sg.InputText(tooltip="Type your task here", key="task_input")
 add_button = sg.Button("Add", tooltip="Click to add the task", key="add_task")
 
 window = sg.Window(
@@ -16,11 +16,15 @@ window = sg.Window(
 while True: 
     event, values = window.read()
     match event:
-        case "Add":
-            todo_list = file_manager.get_todo_list()
-            new_task = values['add_task'] + "\n"
-            todo_list.append(new_task)
-            file_manager.write_todo_list(todo_list)
+        case "add_task":
+            new_task = values['task_input'].strip()
+
+            if new_task:
+                todo_list = file_manager.get_todo_list()
+                todo_list.append(new_task + "\n")
+                file_manager.write_todo_list(todo_list)
+
+                window["task_input"].update("")  # Clear the input box after adding the task
         case sg.WINDOW_CLOSED:
             break
 

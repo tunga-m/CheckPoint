729201545cce926db08a8ed29cec4c955a2d577f
tunga-m <tungamarkal@gmail.com>
2026-09-12 15:33:07 -0400

    implemented "edit" button

--- gui.py
+++ gui.py
@@ -1,15 +1,25 @@
 import FreeSimpleGUI as sg
 
-import actions  # noqa: F401
 import file_manager
 
 label = sg.Text("Enter a task:")
-input_box = sg.InputText(tooltip="Type your task here", key="task_input")
-add_button = sg.Button("Add", tooltip="Click to add the task", key="add_task")
+input_box = sg.InputText(tooltip="Type your task here",
+                         key="task_input")
+
+add_button = sg.Button("Add", 
+                       tooltip="Click to add the task", 
+                       key="add_task")
+
+edit_button = sg.Button("Edit")
+
+list_box = sg.Listbox(values=file_manager.get_todo_list(),
+                      key="todo_list", 
+                      enable_events=True, 
+                      size=(40, 10))
 
 window = sg.Window(
                    "CheckPoint", 
-                   layout=[[label], [input_box, add_button]], 
+                   layout=[[list_box, edit_button], [label], [input_box, add_button]], 
                    font=("Helvetica", 15)
                   )
 
@@ -25,6 +35,24 @@ while True:
                 file_manager.write_todo_list(todo_list)
 
                 window["task_input"].update("")  # Clear the input box after adding the task
+                window["todo_list"].update(values=file_manager.get_todo_list())
+        case "Edit":
+            if values['todo_list']:
+                task_to_edit = values['todo_list'][0]
+                new_task = values['task_input'].strip()
+
+                if new_task:
+                    todo_list = file_manager.get_todo_list()
+                    index = todo_list.index(task_to_edit)
+                    todo_list[index] = new_task + "\n"
+                    file_manager.write_todo_list(todo_list)
+
+                    window["task_input"].update("")  # Clear the input box after editing the task
+                    window["todo_list"].update(values=file_manager.get_todo_list())  # Refresh the listbox
+        case "todo_list":
+            if values['todo_list']:
+                window["task_input"].update(
+                    value=values['todo_list'][0].strip())  # Populate the input box with the selected task
         case sg.WINDOW_CLOSED:
             break
 
